38b76b7a34d66d1bbb90ee333da2791454fb6aa5
urm0922 <285221369+urm0922@users.noreply.github.com>
2026-07-15 10:26:49 +0900

    Before Update

--- app/models/knowledge_article.rb
+++ app/models/knowledge_article.rb
@@ -1,2 +1,10 @@
 class KnowledgeArticle < ApplicationRecord
+  belongs_to :inquiry
+  belongs_to :category
+  belongs_to :author, class_name: "User"
+  enum status: { draft: 0, published: 1, archived: 2 }
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :status, presence: true
 end
--- test/models/knowledge_article_test.rb
+++ test/models/knowledge_article_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class KnowledgesArticleTest < ActiveSupport::TestCase
+class KnowledgeArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
