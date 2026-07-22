1d0199877c578ae440690f8528af3cf4e2f77fbd
urm0922 <285221369+urm0922@users.noreply.github.com>
2026-07-22 19:56:18 +0900

    Update FakeGeneratorTest

--- test/services/inquiries/approve_and_generate_drafts_test.rb
+++ test/services/inquiries/approve_and_generate_drafts_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class Inquiries::ApproveAndGenerateDraftsTest < ActiveSupport::TestCase
-  fixtures :users, :inquiries, :categories, :comments, :knowledge_articles
   FakeGenerator = Struct.new(:result) do
     def call(inquiry:)
       result
@@ -12,10 +11,11 @@ class Inquiries::ApproveAndGenerateDraftsTest < ActiveSupport::TestCase
     inquiry = inquiries(:staff_answered)
     approver = users(:admin)
 
-    assert_difference ["KnowledgeArticle.count", "FaqEntry.count"],1 do
+    assert_difference ["KnowledgeArticle.count", "FaqEntry.count"], 1 do
         Inquiries::ApproveAndGenerateDrafts.new(
             inquiry: inquiry,
-            approver: approver
+            approver: approver,
+            generator: fake_generator
           ).call
     end
 
@@ -25,15 +25,11 @@ class Inquiries::ApproveAndGenerateDraftsTest < ActiveSupport::TestCase
     faq_entry = knowledge_article.faq_entries.reload.first
     
     assert_equal "draft", knowledge_article.reload.status
-    assert_equal inquiry.title, knowledge_article.title
-    assert_equal inquiry.body, knowledge_article.body
     assert_not knowledge_article.published_at?
     assert_equal inquiry.user, knowledge_article.author
     assert knowledge_article.generated_by_ai?
 
     assert_equal "draft", faq_entry.reload.status
-    assert_equal inquiry.title, faq_entry.question
-    assert_equal inquiry.body, faq_entry.answer
     assert faq_entry.generated_by_ai?
   end
 
@@ -43,7 +39,8 @@ class Inquiries::ApproveAndGenerateDraftsTest < ActiveSupport::TestCase
   
     Inquiries::ApproveAndGenerateDrafts.new(
       inquiry: inquiry,
-      approver: approver
+      approver: approver,
+      generator: fake_generator
     ).call
 
     inquiry.reload
